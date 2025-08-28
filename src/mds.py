c4fd3f93c940198ba29612f2b731a758439399d9
Christian Meesters <meesters@uni-mainz.de>
2025-08-28 12:36:34 +0200

    feat: first delegation to nanopub added

--- src/mds.py
+++ src/mds.py
@@ -10,56 +10,71 @@ import sys
 
 import jsonavu
 
-def populate_avu(collection,
-                 avu):
+from metadata import create_data_object_from_json
+from nanopub_handler import create_nanopub, set_debug
+
+
+def populate_avu(collection, avu):
     """
-    performs an 
+    performs an
     'imeta add -C <collection namce> <avu triplet>' call
 
     expects:
       @input collection - the irods collection
       @input avu        - an avu triplet (dict)
     """
-    call = 'imeta add -C {collection} {avu[a]} {avu[v]} {avu[u]}'.format(collection=collection,
-     avu=avu)
+    call = "imeta add -C {collection} {avu[a]} {avu[v]} {avu[u]}".format(
+        collection=collection, avu=avu
+    )
     call = shlex.split(call)
     process = subprocess.Popen(call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
     out = out.decode(locale.getdefaultlocale()[1])
     err = err.decode(locale.getdefaultlocale()[1])
     if process.returncode == 4:
-        print("ERROR: refusing to overwrite existing avu: %s" % avu,
-              file=sys.stderr)
+        print("ERROR: refusing to overwrite existing avu: %s" % avu, file=sys.stderr)
     elif process.returncode:
-        print("call failed, call was: %s" % ' '.join(call))
+        print("call failed, call was: %s" % " ".join(call))
         print("Message was: %s" % str(out))
         print("Error code was %s, stderr: %s" % (process.returncode, err))
     return process.returncode, out, err
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    infiles = parser.add_argument_group('Defining input Files')
-    infiles.add_argument('-j', '--json', help="defines json schema input")
-    
-    irods = parser.add_argument_group('iRODs related Information')
-    irods.add_argument('-c', '--collection', help="the iRODS collection to annotate")
-    
+    infiles = parser.add_argument_group("Defining input Files")
+    infiles.add_argument("-j", "--json", help="defines json schema input")
+
+     
+    nanopub = parser.add_argument_group("Nanopublication related Information")
+    nanopub.add_argument("--real", action="store_true", help="do not use the nanopub testnet",
+                         default=False)
+    nanopub.add_argument("-n", "--nanopub", action="store_true", help="create a nanopub")
+
+    irods = parser.add_argument_group("iRODs related Information")
+    irods.add_argument("-c", "--collection", help="the iRODS collection to annotate")
+
+    utils = parser.add_argument_group("Utility options")
+    utils.add_argument("-d", "--debug", action="store_true", help="enable debugging output",
+                       default=False)
+
     args = parser.parse_args()
 
-    # load the data
-    if os.path.exists(args.json):
-       with open(args.json) as fname:
-           data = json.load(fname)
-    else:
-       raise IOError("JSON file '%s' not found" % args.json)
+    if args.debug:
+        set_debug(True)
 
-    avus  = jsonavu.json2avu(data, 'root')
+    data_object = create_data_object_from_json(args.json)
+    #print(data_object)
+    #sys.exit(0)
+  
+    #avus = jsonavu.json2avu(data, "root")
 
-    for triplet in avus:
-        populate_avu(args.collection,
-                     triplet)
-
-
+    if args.nanopub:
+        create_nanopub(data_object, use_testnet=not args.real,
+                       debug_flag=args.debug)
 
 
+    
 
+#    for triplet in avus:
+#        populate_avu(args.collection, triplet)
