8990dd3a1ae28fd47a04dd3af5fc02a4b0782a42
Christian Meesters <meesters@uni-mainz.de>
2025-08-28 12:37:03 +0200

    feat: added first abstract metadata handling function

---
+++ src/metadata.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+def create_data_object_from_json(json_fname):
+    
+    """
+    Create a data object from a JSON file.
+
+    :param json_fname: The path to the JSON file.
+    :return: The data object created from the JSON file.
+    """
+  # load the data
+    if os.path.exists(json_fname):
+        with open(json_fname) as fname:
+            data = json.load(fname)
+    else:
+        raise IOError("JSON file '%s' not found" % json_fname)
+    return data
